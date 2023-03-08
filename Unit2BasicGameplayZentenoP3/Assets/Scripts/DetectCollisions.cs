0896c3fdbfd678a5c9ba09daeb8033240e513ee0
d3trim3ntal <obsidianblackrose@gmail.com>
2023-03-08 11:40:33 -0800

    Doing the Bonus features then will be done
    almost done

--- Unit2BasicGameplayZentenoP3/Assets/Scripts/DetectCollisions.cs
+++ Unit2BasicGameplayZentenoP3/Assets/Scripts/DetectCollisions.cs
@@ -6,8 +6,16 @@ public class DetectCollisions : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-        Destroy(other.gameObject);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
     }
     // Start is called before the first frame update
     void Start()
